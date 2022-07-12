fb0d142627c540d90c220f5d5b41ea42fbc20582
Brandon Spell <brandon.spell@rackspace.com>
2022-07-12 13:56:05 -0500

    Refactor Get-TerraformInstallVersionList

--- TFSwitchPS/Private/Get-TerraformInstalledVersionList.ps1
+++ TFSwitchPS/Private/Get-TerraformInstalledVersionList.ps1
@@ -1,33 +1,59 @@
 Function Get-TerraformInstalledVersionList {
     [CmdletBinding()]
     param (
+
         [Parameter()]
         [string]
         $Version
+
     )
-    Write-Verbose "Retrieving List of Installed Terraform Versions"
+    Begin {
 
-    $tfVersions = Get-ChildItem $env:TFSWITCH_BASEDIR -Recurse -File -Filter 'terraform.exe'
-    if ( !$tfversions ) {
-        Write-Warning "No Terraform Install found in location $env:TFSWITCH_BASEDIR"
-    } elseif ( $Version ) {
-        Write-Verbose "Filtering for Version [$Version]"
-    }
+        Write-Verbose "[$($MyInvocation.MyCommand)] Starting Function"
     
-    $return = $tfVersions | ForEach-Object {
-        $versionTemp = Split-Path $_ -Parent | Split-Path -Leaf
-        New-Object psobject -Property ([ordered]@{
-            Version = $versionTemp
-            isActive = $versionTemp -eq $env:TFSWITCH_VERSION
-            Path = $_.FullName
-        })
     }
-    if ( $Version ) {
-        $return = $return | Where-Object { $_.Version -eq $Version }
-        if ( !$return ) {
-            Write-Warning "[$Version] version not found locally."
-        }
-    }
+    Process {
 
-    return $return
+        $tfVersions = Get-ChildItem $env:TFSWITCH_BASEDIR -Recurse -File -Filter 'terraform.exe' -ErrorAction SilentlyContinue
+
+        if ( !$tfversions ) {
+
+            Write-Warning "No Terraform Install found in location $env:TFSWITCH_BASEDIR"
+
+
+        } elseif ( $Version ) {
+
+            Write-Verbose "[$($MyInvocation.MyCommand)] Filtering for Version [$Version]"
+
+        }
+        
+        $return = $tfVersions | ForEach-Object {
+
+            $versionTemp = Split-Path $_ -Parent | Split-Path -Leaf
+
+            New-Object psobject -Property ([ordered]@{
+
+                Version = $versionTemp
+
+                isActive = $versionTemp -eq $env:TFSWITCH_VERSION
+
+                Path = $_.FullName
+
+            })
+        }
+
+        if ( $Version ) {
+
+            $return = $return | Where-Object { $_.Version -eq $Version }
+
+            if ( !$return ) {
+
+                Write-Warning "[$Version] version not found locally."
+
+            }
+        }
+    
+        return $return
+
+    }
 }
