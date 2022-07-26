16a9c9f91cbf3da7e9868c8612f6badadeb3e6cf
Brandon Spell <brandon.spell@rackspace.com>
2022-07-26 09:56:00 -0500

    Refactor Get-TerraformRemoteVersionList

--- TFSwitchPS/Private/Get-TerraformRemoteVersionList.ps1
+++ TFSwitchPS/Private/Get-TerraformRemoteVersionList.ps1
@@ -5,37 +5,33 @@ Function Get-TerraformRemoteVersionList{
         [string]
         $Version
     )
-    Function parseVersion($htmlInput){
-        $versionName =$htmlInput.trim().TrimEnd('</a>').Split('>')[1]
-        $version = $htmlInput.TrimStart().Split('"')[1].Split('/').Where({$_})[-1]
-        $versionPath = $htmlInput.Trim().Split('"')[1]
-        $os = if($IsWindows){'windows'}elseif($IsLinux){'linux'}else{throw 'invalid OS'}
-        $arch = if([Environment]::Is64BitOperatingSystem){'amd64'}else{'386'}
-
-        $href = 'https://releases.hashicorp.com{0}{1}_{2}_{3}.zip' -f $versionPath, $versionName,$os,$arch
-        New-Object psobject -Property ([ordered]@{
-            Version = $htmlInput.TrimStart().Split('"')[1].Split('/').Where({$_})[-1]
-            Name = $versionName
-            isInstalled = [bool](Get-TerraformInstalledVersionList -Version $version -Verbose:$false -WarningAction SilentlyContinue)
-            Link = $href
-        })
-    }
-    $splat = @{
-        Uri = 'https://releases.hashicorp.com/terraform'
-        Verbose = $false
-        ErrorAction = 'Stop'
-    }
-    $versionPattern = "<a href=`"/terraform/\d{1,}\.\d{1,}\.\d{1,}\/"
-    try {
-        $tfReturn = Invoke-WebRequest @splat
-
-        if ( $tfReturn.StatusCode -eq '200') {
-            $tfReturn.Content -split "`n" | 
-                Where-Object { $_ -match $versionPattern -and $_ -like "*$Version*"} |
-                    ForEach-Object { parseVersion $_ } |
-                        Where-Object {if($Version){$_.Version -eq $Version}else{$true}}
+    Begin {
+        $splat = @{
+            Uri = 'https://releases.hashicorp.com/terraform'
+            Verbose = $false
+            ErrorAction = 'Stop'
         }
-    } catch {
-        Write-Warning $($_.Exception.Message)
+        $versionPattern = "<a href=`"/terraform/\d{1,}\.\d{1,}\.\d{1,}\/"
+
+    }
+    Process {
+        try {
+            $tfReturn = Invoke-WebRequest @splat
+
+            if ( $tfReturn.StatusCode -eq '200') {
+                $tfReturn.Content -split "`n" |
+                    Where-Object { $_ -match $versionPattern -and $_ -like "*$Version*"} |
+                        ForEach-Object { Convertfrom-TerraformHTML $_ } |
+                            Where-Object {
+                                if ( $Version ) {
+                                    $_.Version -like $Version
+                                } else {
+                                    $true
+                                }
+                            }
+            }
+        } catch {
+            Write-Warning $($_.Exception.Message)
+        }
     }
 }
