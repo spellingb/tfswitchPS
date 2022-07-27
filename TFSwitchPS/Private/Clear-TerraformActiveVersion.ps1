275a7d506b7b01bce592c985520d746ad62c898c
Brandon Spell <brandon.spell@rackspace.com>
2022-07-26 22:26:55 -0500

    Refactor Clear-TerraformActiveVersion

--- TFSwitchPS/Private/Clear-TerraformActiveVersion.ps1
+++ TFSwitchPS/Private/Clear-TerraformActiveVersion.ps1
@@ -1,13 +1,16 @@
 Function Clear-TerraformActiveVersion {
     [CmdletBinding()]
-    # [alias('Clear-TFActiveVersion')]
+    [OutputType([void])]
     param (
     )
-    Write-Verbose "Clearing Active Terraform Version State"
-    Remove-Item Env:\TFSWITCH_PATH -ErrorAction SilentlyContinue
-    Remove-Item Env:\TFSWITCH_VERSION -ErrorAction SilentlyContinue
-    $pathTemp = $env:Path.Clone()
-    $env:Path = ($pathTemp.Split(';') | Where-Object {$_ -notmatch "\.terraform" }) -join ';'
-    [System.Environment]::SetEnvironmentVariable( 'TFSWITCH_VERSION', $null, [System.EnvironmentVariableTarget]::User)
-    [System.Environment]::SetEnvironmentVariable('path', $env:Path ,[System.EnvironmentVariableTarget]::User)
+    Process {
+        Write-Verbose "[$($MyInvocation.MyCommand)] Starting Function"
+        Write-Verbose "Clearing Active Terraform Version State"
+        Remove-Item Env:\TFSWITCH_PATH -ErrorAction SilentlyContinue
+        Remove-Item Env:\TFSWITCH_VERSION -ErrorAction SilentlyContinue
+        $pathTemp = $env:Path.Clone()
+        $env:Path = ($pathTemp.Split(';') | Where-Object {$_ -notmatch "\.terraform" }) -join ';'
+        [System.Environment]::SetEnvironmentVariable( 'TFSWITCH_VERSION', $null, [System.EnvironmentVariableTarget]::User)
+        [System.Environment]::SetEnvironmentVariable('path', $env:Path ,[System.EnvironmentVariableTarget]::User)
+    }
 }
